e8995e505eba8e8a03a3e008d5edb0e37b2ac0f3
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2023-02-23 12:43:30 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,5 @@
-// import 'dart:html'; // ignore: avoid_web_libraries_in_flutter
-
-// import 'package:flutter/widgets.dart';
 import 'package:tryout_game_maikel/collect_money/main.dart' as collect_money;
 
 void main() {
-  // var page = window.location.search ?? '';
-  // if (page.startsWith('?')) {
-  //   page = page.substring(1);
-  // }
-
-  // switch (page) {
-  //   case 'collect_money':
-  //     collect_money.main();
-  //     break;
-
-  //   default:
-      // runApp(
-      //   Directionality(
-      //     textDirection: TextDirection.ltr,
-      //     child: Text('Error: unknown page name "$page"'),
-      //   ),
-      // );
-      collect_money.main();
-  // }
+  collect_money.main();
 }
